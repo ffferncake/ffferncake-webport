919e8a64f047c24755a86ee35126635fc66f54f8
ffferncake <59507098+ffferncake@users.noreply.github.com>
2022-04-02 21:36:35 +0700

    1st commit

---
+++ Images/card.js
@@ -0,0 +1,11 @@
+function getInTouch() {
+  var name = "Nichanun Trakulphudphong";
+  var title = "GIS data engineer/Fronted devloper";
+  var phone = "+66 623 565 648";
+  var email = "fernnichanun@gmail.com";
+}
+getInTouch();
+
+
+
+
